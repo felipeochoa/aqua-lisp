e63096c0fbd9a18f9062f9eb785d46df6e19d2cc
unknown <22FOA@CHIX1C2-22FOA.BAIN.com>
2014-10-24 17:32:45 -0500

    created the py2lisp base

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ py-lisp
 =======
 
 Python to Common Lisp transaltor and runtime
+
+See [felipeochoa.github.io/py-lisp](felipeochoa.github.io/py-lisp) for
+a detailed description and documentation
---
+++ py2lisp.py
@@ -0,0 +1,40 @@
+#!python
+
+"""
+This module translates Python source into Lisp forms.
+
+The generated Lisp is little more than a Python-heavy DSL which depends on a
+Lisp runtime for all the macro/function definitions.
+
+"""
+
+import ast
+
+
+def translate(node_or_literal):
+    "Convert an AST node or Python literal into a Lisp form (recursively)."
+    if isinstance(node_or_literal, ast.AST):
+        symbol = "|py-%s|" % node_or_literal.__class__.__name__
+        if not node_or_literal._fields:  # this is a leaf node
+            return symbol
+        args = " ".join(translate(sub_nl)
+                        for _, sub_nl in ast.iter_fields(node_or_literal))
+        return "(%s %s)" % (symbol, args)
+    return translate_literal(node_or_literal)
+
+
+def translate_literal(literal):
+    "Translate a Python literal into a Lisp literal."
+    if isinstance(literal, str):
+        return '"%s"' % literal
+    elif isinstance(literal, bytes):
+        return "#(%s)" % " ".join(int.from_bytes(b, "big")
+                                       for b in literal)
+    elif literal is None:
+        return "|None|"
+    elif isinstance(literal, bool):
+        return "t" if literal else "nil"
+    elif isinstance(literal, list):
+        return "(%s)" % " ".join(map(translate, literal))
+    else:  # Should be a number
+        return str(literal)
