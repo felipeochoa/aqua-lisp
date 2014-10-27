19287bb542c3829ecceb7e0c6f915614365c0c72
unknown <22FOA@CHIX1C2-22FOA.BAIN.com>
2014-10-26 21:05:18 -0500

    added tests for translating nodes

--- test_py2lisp.py
+++ test_py2lisp.py
@@ -1,37 +1,40 @@
 #!python
 
 import unittest
-from py2lisp import translate_literal
+import ast
+from py2lisp import translate, translate_literal
 
 class TestLiterals(unittest.TestCase):
 
+    function = staticmethod(translate_literal)
+
     def test_string(self):
-        self.assertEqual("#()", translate_literal(""))
-        self.assertEqual("#(0 0 0 97)", translate_literal("a"))
-        self.assertEqual("#(0 0 0 97 0 0 0 98)", translate_literal("ab"))
+        self.assertEqual("#()", self.function(""))
+        self.assertEqual("#(0 0 0 97)", self.function("a"))
+        self.assertEqual("#(0 0 0 97 0 0 0 98)", self.function("ab"))
         self.assertEqual("#(0 0 0 97 0 0 0 98 0 0 0 99)",
-                          translate_literal("abc"))
-        self.assertEqual("#(0 16 1 0)", translate_literal("\U00100100"))
+                          self.function("abc"))
+        self.assertEqual("#(0 16 1 0)", self.function("\U00100100"))
 
     def test_int(self):
-        self.assertEqual("-23", translate_literal(-23))
-        self.assertEqual("0", translate_literal(0))
-        self.assertEqual("5", translate_literal(5))
+        self.assertEqual("-23", self.function(-23))
+        self.assertEqual("0", self.function(0))
+        self.assertEqual("5", self.function(5))
         self.assertEqual("99991152921504606846975",
-                         translate_literal(99991152921504606846975))
+                         self.function(99991152921504606846975))
         self.assertEqual("-99991152921504606846975",
-                         translate_literal(-99991152921504606846975))
+                         self.function(-99991152921504606846975))
 
     def test_bytes(self):
-        self.assertEqual("#()", translate_literal(b""))
-        self.assertEqual("#(97)", translate_literal(b'\x61'))
-        self.assertEqual("#(97 98)", translate_literal(b'\x61\x62'))
-        self.assertEqual("#(97 98 99)", translate_literal(b'\x61\x62\x63'))
+        self.assertEqual("#()", self.function(b""))
+        self.assertEqual("#(97)", self.function(b'\x61'))
+        self.assertEqual("#(97 98)", self.function(b'\x61\x62'))
+        self.assertEqual("#(97 98 99)", self.function(b'\x61\x62\x63'))
 
     def test_float(self):
         # Example from http://stackoverflow.com/questions/3481289/
         f = 0.38288746115497402
-        self.assertEqual(f, float(translate_literal(f)))
+        self.assertEqual(f, float(self.function(f)))
 
     def test_complex(self):
         import re
@@ -40,7 +43,7 @@ class TestLiterals(unittest.TestCase):
                                 (float_pattern, float_pattern))
         f = 0.38288746115497402
         def test_complex(complex_):
-            match = complex_re.match(translate_literal(complex_))
+            match = complex_re.match(self.function(complex_))
             self.assertTrue(match)
             self.assertEqual(float(match.group(1)), complex_.real)
             self.assertEqual(float(match.group(2)), complex_.imag)
@@ -49,18 +52,59 @@ class TestLiterals(unittest.TestCase):
         test_complex(f + f * 1j)
 
     def test_None(self):
-        self.assertEqual("|None|", translate_literal(None))
+        self.assertEqual("|None|", self.function(None))
 
     def test_bool(self):
-        self.assertEqual("t", translate_literal(True))
-        self.assertEqual("nil", translate_literal(False))
+        self.assertEqual("t", self.function(True))
+        self.assertEqual("nil", self.function(False))
 
     def test_list(self):
-        self.assertEqual("()", translate_literal([]))
-        self.assertEqual("(1 2 3)", translate_literal([1, 2, 3]))
+        self.assertEqual("()", self.function([]))
+        self.assertEqual("(1 2 3)", self.function([1, 2, 3]))
         self.assertEqual("(#(97) 1 #(0 0 0 98) |None| t nil)",
-                         translate_literal([b'\x61', 1, 'b',
-                                            None, True, False]))
+                         self.function([b'\x61', 1, 'b',
+                                        None, True, False]))
+
+
+class TestTranslate(TestLiterals):
+
+    function = staticmethod(translate)
+
+    def test_leaf_node(self):
+        self.assertEqual("(|py-Gt|)", self.function(ast.Gt()))
+        self.assertEqual("(|py-Load|)", self.function(ast.Load()))
+
+    def test_tree_node(self):
+        self.assertEqual("(|py-Bytes| #(97 98))",
+                         self.function(ast.Bytes(b'\x61\x62')))
+        self.assertEqual("(|py-Str| #(0 0 0 97))",
+                          self.function(ast.Str('a')))
+        self.assertEqual("(|py-Num| 97)", self.function(ast.Num(97)))
+        self.assertEqual("(|py-NameConstant| t)",
+                         self.function(ast.NameConstant(True)))
+        self.assertEqual("(|py-NameConstant| nil)",
+                         self.function(ast.NameConstant(False)))
+        self.assertEqual("(|py-NameConstant| |None|)",
+                         self.function(ast.NameConstant(None)))
+        self.assertEqual("(|py-Module| " +
+                         "((|py-Expr| (|py-NameConstant| |None|))" +
+                         " (|py-Expr| (|py-NameConstant| |None|))" +
+                         " (|py-Expr| (|py-NameConstant| |None|))))",
+                         self.function(ast.Module(
+                             [ast.Expr(ast.NameConstant(None))] * 3)))
+
+    def test_parsing(self):
+        [("None", "(|py-Module| ((|py-Expr| (|py-NameConstant| |None|))))"),
+         ("3", "(|py-Module| ((|py-Expr| (|py-Num| 3))))"),
+         ("'abc'", "(|py-Module| ((|py-Expr| (|py-Str| "+
+          "#(0 0 0 97 0 0 0 98 0 0 0 99)))))"),
+         ("if a:\n  return b\nelse:\n  continue", (
+             "(|py-Module|" +
+             " (|py-If| (|py-Name| #(0 0 0 97) (|py-Load|))" +
+              " ((|py-Return| (|py-Name| #(0 0 0 98) (|py-Load|))))" +
+              " ((|py-Continue|))))"))
+        ]
+
 
 if __name__ == "__main__":
     unittest.main()
